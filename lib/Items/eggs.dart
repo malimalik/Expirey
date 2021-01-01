9995e2d3ec8c79d70b637df344365fd29e0577d4
Ali Malik <ialimalik99@gmail.com>
2021-01-01 18:39:31 -0500

    Added placeholder text for available quantity

--- lib/Items/eggs.dart
+++ lib/Items/eggs.dart
@@ -6,6 +6,8 @@ class Eggs extends StatefulWidget {
   _EggsState createState() => _EggsState();
 }
 
+Color textColor = new Color(707070);
+
 class _EggsState extends State<Eggs> {
   @override
   Widget build(BuildContext context) {
@@ -14,36 +16,68 @@ class _EggsState extends State<Eggs> {
         onPressed: () {},
         tooltip: ('Add a new Transaction'),
       ),
-      body: ListView(children: [
-        new Column(
-          children: [
-            new Container(
-              child: new Image(image: AssetImage('Assets/eggs.png')),
-            ),
-            Align(
-              child: Padding(
-                padding: const EdgeInsets.only(top: 8),
-                child: Text(
-                  'Eggs',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Color.fromRGBO(70, 70, 70, 1),
-                      fontSize: 44),
+      body: ListView(
+        children: [
+          new Column(
+            children: [
+              new Container(
+                child: new Image(image: AssetImage('Assets/eggs.png')),
+              ),
+              Align(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Text(
+                    'Eggs',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromRGBO(70, 70, 70, 1),
+                        fontSize: 44),
+                  ),
                 ),
+                alignment: Alignment.center,
               ),
-              alignment: Alignment.center,
-            ),
-            Align(
-              child: new Image(
-                image: AssetImage('Assets/red-line-png-0.png'),
-                width: 30,
-                height: 25,
+              Align(
+                child: new Image(
+                  image: AssetImage('Assets/red-line-png-0.png'),
+                  width: 30,
+                  height: 25,
+                ),
+                alignment: Alignment.center,
               ),
-              alignment: Alignment.center,
-            ),
-          ],
-        )
-      ]),
+              Column(
+                children: [
+                  Padding(
+                      padding: const EdgeInsets.only(left: 10),
+                      child: Row(
+                        children: [
+                          Align(
+                            alignment: Alignment.bottomLeft,
+                            child: Text(
+                              'Available',
+                              style: TextStyle(
+                                  fontSize: 25,
+                                  color: Color.fromRGBO(70, 70, 70, 1)),
+                              textAlign: TextAlign.left,
+                            ),
+                          ),
+                          Expanded(
+                            child: Padding(
+                              padding: EdgeInsets.only(right: 15),
+                              child: Text('2',
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      color: Color.fromRGBO(70, 70, 70, 1)),
+                                  textAlign: TextAlign.right),
+                            ),
+                          ),
+                        ],
+                      )),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
