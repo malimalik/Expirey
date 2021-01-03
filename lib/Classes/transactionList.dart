2630d642a065e4ee5c50bc7558650719826c01c2
Ali Malik <ialimalik99@gmail.com>
2021-01-02 19:41:26 -0500

    Added ternary statement for the transactions list

--- lib/Classes/transactionList.dart
+++ lib/Classes/transactionList.dart
@@ -3,6 +3,7 @@ import 'package:Expirey/Classes/transaction.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
+
   Function deleteTransactions;
   final String item;
   TransactionList(this.transactions, this.deleteTransactions, this.item);
@@ -10,27 +11,24 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: MediaQuery.of(context).size.height *0.6,
-
-      child: transactions.isEmpty ?
-      Column(
-        children: [
-          Container(
-         child: Image(image: AssetImage('Assets/out_of_stock.png')),
-        
-        ),
-
-        Container(
-          padding: EdgeInsets.all(2),
-          child: Text('It appears that you are all out of stock for ' +item),
-
-          
-          )]
-        
-
-
-      ),
-    
-   );
+        height: MediaQuery.of(context).size.height * 0.6,
+        child: transactions?.isEmpty ?? true
+            ? Column(
+                children: <Widget>[
+                  Container(
+                    child: Text(
+                      'It appears that you have no transactions yet, press the add button on the bottom right corner to get started.',
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  Image.network(
+                    'https://www.pngitem.com/pimgs/m/434-4341211_transparent-guy-thinking-png-illustration-question-mark-idea.png',
+                    height: 200,
+                    width: 200,
+                    alignment: Alignment.center,
+                  ),
+                ],
+              )
+            : ListView.builder(itemBuilder: null));
   }
 }
