d4b08dba8b0d64ee9501515e006cf9cf0f0eac68
Ali Malik <ialimalik99@gmail.com>
2021-01-14 14:17:28 -0500

    comment on json map

--- lib/Classes/transaction.dart
+++ lib/Classes/transaction.dart
@@ -1,40 +1,34 @@
+/// The class that allows to construct a new instance in the database and add a new transaction (item in the stock).
 import 'package:firebase_database/firebase_database.dart';
 
-/// This class has the constructor for the transactions.
-///
-/// The transactions are basically
 import 'package:flutter/material.dart';
 import 'dart:ui';
 
 class Transaction {
-
-
   DatabaseReference id;
   final DateTime expirationDate;
   final String category;
   final String itemName;
   int quantity;
 
-  ///
-  /// This constructor allows
+  /// Constructor
   Transaction(
       {@required this.expirationDate,
       @required this.category,
       @required this.itemName,
       @required this.quantity});
 
-  /// This method will be used for setting the id for a specific transactions
+  /// This method will be used for setting the id everytime a new transaction is made.
   void setId(DatabaseReference id) {
     this.id = id;
   }
-
-  Map<String, dynamic> toJson()
-  {
+  /// A map is created here with key value pairs which allows to convert to Json. This ensures uniqueness in the databse.
+  Map<String, dynamic> toJson() {
     return {
       'expirationDate': this.expirationDate,
       'category': this.category,
       'itemName': this.itemName,
       'quantity': this.quantity,
     };
-  } 
+  }
 }
